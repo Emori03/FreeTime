db0bd5a6cfc560837cc97f443cb21999463ae8d8
Emori03 <emorijan@gmail.com>
2024-11-21 23:40:37 -0600

    Modulos
    Integracion de la carga de modulos dinamicos

--- FreeTimeBD.sql
+++ FreeTimeBD.sql
@@ -4,3 +4,19 @@ CREATE TABLE users(
     UserEmail VARCHAR(255) COMMENT 'Correo electronico del usuario',
     UserPassword VARCHAR(255) COMMENT 'Contraseña del usuario'
 ) COMMENT '';
+
+CREATE TABLE productos(
+    ProductId INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    ProductName VARCHAR(255) COMMENT 'Nombre del producto',
+    ProductDescription VARCHAR(255) COMMENT 'Descripción del producto',
+    ProductPrice DECIMAL(10,2) COMMENT 'Precio del producto'
+) COMMENT 'Lista de productos';
+
+CREATE Table productosDetalles(
+    ProductId INT NOT NULL COMMENT 'ProductoID',
+    ProductStock INT NOT NULL COMMENT 'Existencia de productos'
+) COMMENT 'Detalles de los productos';
+
+ALTER TABLE productosDetalles
+ADD CONSTRAINT FK_Productos_ProductosDetalles
+FOREIGN KEY (ProductId) REFERENCES productos(ProductId);
