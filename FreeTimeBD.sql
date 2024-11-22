2a2edabbbdd585e0c38387bbc9a68a2cf8101246
Emori03 <emorijan@gmail.com>
2024-11-22 00:19:06 -0600

    Docker File
    Agregar Docker File para desplegar

--- FreeTimeBD.sql
+++ FreeTimeBD.sql
@@ -1,6 +1,13 @@
+CREATE DATABASE freetime;
+
+USE freetime;
+
 CREATE TABLE users(  
     UserId int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
     UserName VARCHAR(255) COMMENT 'Nombre del usuario',
     UserEmail VARCHAR(255) COMMENT 'Correo electronico del usuario',
     UserPassword VARCHAR(255) COMMENT 'Contraseña del usuario'
 ) COMMENT '';
+
+INSERT INTO users(UserName, UserEmail, UserPassword)
+VALUES ('test', 'test@test.com', 'test09');
